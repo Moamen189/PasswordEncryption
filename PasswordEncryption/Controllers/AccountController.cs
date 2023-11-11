e34fbb0a354b63b41f1bea9c85206b49ecbd4476
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-11-11 03:18:04 +0200

    Add project files.

---
+++ PasswordEncryption/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordEncryption.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+    }
+}
